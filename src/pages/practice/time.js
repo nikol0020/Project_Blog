072340219ec2ff202656e5d643250e423f6a33bf
Tetiana Chulkova <Tetiana.Chulkova@cantire.com>
2022-10-09 00:27:30 +0300

    HW_08: added clock on the home-page

--- src/pages/practice/time.js
+++ src/pages/practice/time.js
@@ -11,3 +11,40 @@ function currentDate(){
 }
 
 window.setInterval(currentDate, 1000);
+
+function count() {
+    let seconds = 0,
+        minutes = 0,
+        minute = 0;
+
+    document.getElementById('minutes').innerHTML = `0${minutes} : `;
+    document.getElementById('seconds').innerHTML = `0${seconds}`;
+
+    return ( () => {
+        ++seconds;
+        if ( seconds % 60 === 0) {
+            ++ minutes;
+            seconds = 0;
+            minute = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+            document.getElementById('minutes').innerHTML = `${minute} : `;
+        }
+        seconds = (seconds < 10) ? `0${seconds}` : `${seconds}`;
+        document.getElementById('seconds').innerHTML = `${seconds}`;
+    })
+}
+
+function timeOnPage() {
+    let getDuration = count();
+    let elem = document.getElementById('time');
+    let timerId = setInterval(getDuration, 1000);
+
+    elem.addEventListener('mouseover', (e) => {
+        clearInterval(timerId);
+    });
+
+    elem.addEventListener('mouseout', (e) => {
+        timerId = setInterval(getDuration, 1000);
+    });
+}
+
+timeOnPage();
